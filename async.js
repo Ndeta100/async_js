ea4b14756aef5ba44fd8a15bb8ec72fd5fbc3d30
Ndeta Innocent <you@example.com>
2022-01-26 11:42:00 +0200

    worker threads

--- async.js
+++ async.js
@@ -143,3 +143,23 @@ Promise.all(urls.map(url=>{
          return `Parallel is done ${output1} ${output2} ${output3}`
      }
      parallel.then(()=>console.log())
+
+
+     const p1 = new Promise((resolve, reject) => {
+        setTimeout(() => resolve("A"), Math.floor(Math.random() * 1000));
+      });
+      const p2 = new Promise((resolve, reject) => {
+        setTimeout(() => resolve("B"), Math.floor(Math.random() * 1000));
+      });
+      const p3 = new Promise((resolve, reject) => {
+        setTimeout(() => resolve("C"), Math.floor(Math.random() * 1000));
+      });
+      (async function () {
+        const result = await Promise.any([p1, p2, p3]);
+        console.log(result); // Prints "A", "B" or "C"
+      })();
+
+      var worker=new Worker('worker.js')
+      worker.postMessage('hello')
+
+      addEventListener('message')
