7528b5dd52fb29b39dd2584e112565115fad098d
Ndeta Innocent <you@example.com>
2022-01-26 11:18:35 +0200

    parallel and sequences

--- async.js
+++ async.js
@@ -129,3 +129,17 @@ Promise.all(urls.map(url=>{
              console.log(data)
          }
      }
+
+     const promisify=(item, delay)=>{
+         new Promise((resolve)=>setTimeout(()=>resolve(item), delay))
+     }
+     const a=()=>promisify('a', 100)
+     const b=()=>promisify('b', 5000)
+     const c=()=>promisify('c', 3000)
+
+     async function parallel(){
+         const promises=[a(), b(), c()]
+         const [output1, output2, output3] =await Promise.all(promises)
+         return `Parallel is done ${output1} ${output2} ${output3}`
+     }
+     parallel.then(()=>console.log())
